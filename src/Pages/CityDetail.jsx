5e4a135a0f287ffeb036dae843f91c67f5ab81ff
EmmanuelCime <ruzdemmanuel@outlook.com>
2024-11-05 17:28:09 -0600

    Se agregó el apartado de Itineraries

--- src/Pages/CityDetail.jsx
+++ src/Pages/CityDetail.jsx
@@ -51,67 +51,79 @@ function CityDetail() {
     }
 
     return (
-        <div className="flex flex-col items-center lg:h-[1100px] md:h-[650px] h-[435px] bg-gradient-to-b from-sky-950 to-sky-800 pb-3">
-            <div className="flex justify-center relative md:w-full h-full w-[310px]">
-                <img
-                    src={city.photo}
-                    alt={city.name}
-                    className="xl:w-[1200px] xl:h-[1050px] lg:h-[1020px] lg:w-[900px] md:w-[750px] md:h-[590px] w-[310px] h-[380px] object-cover rounded-lg shadow-lg mt-5"
-                />
+        <div className="flex flex-col items-center justify-center bg-gradient-to-b from-sky-950 to-sky-800 pb-3">
+            <div className="flex flex-col items-center lg:h-[1100px] md:h-[650px] h-[435px]">
+                <div className="flex justify-center relative md:w-full h-full w-[310px]">
+                    <img
+                        src={city.photo}
+                        alt={city.name}
+                        className="xl:w-[1200px] xl:h-[1050px] lg:h-[1020px] lg:w-[900px] md:w-[750px] md:h-[590px] w-[310px] h-[380px] object-cover rounded-lg shadow-lg mt-5"
+                    />
 
-                <div className="flex flex-col items-center justify-center lg:gap-5 gap-1 absolute inset-0 rounded-lg">
-                    <div className="bg-gray-300 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl md:mb-5 mb-2 w-fit">
-                        <h2 className="xl:text-4xl lg:text-2xl md:text-lg text-md font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-yellow-600 md:p-1">{city.name}</h2>
+                    <div className="flex flex-col items-center justify-center lg:gap-5 gap-1 absolute inset-0 rounded-lg">
+                        <div className="bg-gray-300 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl md:mb-5 mb-2 w-fit">
+                            <h2 className="xl:text-4xl lg:text-2xl md:text-lg text-md font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-yellow-600 md:p-1">{city.name}</h2>
+                        </div>
+
+                        <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
+                            <p className="xl:text-lg lg:text-base text-xs font-bold text-white">{city.description}</p>
+                        </div>
+
+                        <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
+                            <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Country:</strong> {city.country}</p>
+                        </div>
+
+                        <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
+                            <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Continent:</strong> {city.continent}</p>
+                        </div>
+
+                        <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
+                            <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Language:</strong> {city.language}</p>
+                        </div>
+
+                        <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
+                            <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Currency:</strong> {city.currency}</p>
+                        </div>
+
+                        <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
+                            <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Time Zone:</strong> {city.timeZone}</p>
+                        </div>
+
+                        <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
+                            <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Population:</strong> {city.population}</p>
+                        </div>
+
+                        <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
+                            <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Latitude:</strong> {city.latitude}</p>
+                        </div>
+
+                        <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
+                            <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Longitude:</strong> {city.longitude}</p>
+                        </div>
+
+                        <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-2 p-1 rounded-2xl text-center">
+                            <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Area Code:</strong> {city.areaCode}</p>
+                        </div>
+
+                        <NavLink
+                            to={`/cities`}
+                            className="lg:p-4 md:p-2 p-1 lg:mt-9 mt-5 lg:text-lg text-xs bg-blue-800 hover:bg-blue-950 text-white font-bold rounded">
+                            Back to Cities
+                        </NavLink>
+
                     </div>
-
-                    <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
-                        <p className="xl:text-lg lg:text-base text-xs font-bold text-white">{city.description}</p>
-                    </div>
-
-                    <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
-                        <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Country:</strong> {city.country}</p>
-                    </div>
-
-                    <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
-                        <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Continent:</strong> {city.continent}</p>
-                    </div>
-
-                    <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
-                        <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Language:</strong> {city.language}</p>
-                    </div>
-
-                    <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
-                        <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Currency:</strong> {city.currency}</p>
-                    </div>
-
-                    <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
-                        <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Time Zone:</strong> {city.timeZone}</p>
-                    </div>
-
-                    <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
-                        <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Population:</strong> {city.population}</p>
-                    </div>
-
-                    <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
-                        <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Latitude:</strong> {city.latitude}</p>
-                    </div>
-
-                    <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-3 p-1 rounded-2xl text-center">
-                        <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Longitude:</strong> {city.longitude}</p>
-                    </div>
-
-                    <div className="bg-gray-950 bg-opacity-80 lg:p-4 md:p-2 p-1 rounded-2xl text-center">
-                        <p className="xl:text-lg lg:text-base text-xs text-white"><strong>Area Code:</strong> {city.areaCode}</p>
-                    </div>
-
-                    <NavLink
-                        to={`/cities`}
-                        className="lg:p-4 md:p-2 p-1 lg:mt-9 mt-5 lg:text-lg text-xs bg-blue-800 hover:bg-blue-950 text-white font-bold rounded">
-                        Back to Cities
-                    </NavLink>
-
                 </div>
+
             </div>
+
+            <div className="flex justify-center md:pt-12 pt-8">
+
+                <h2 className="text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-300 to-yellow-400 md:pb-14 pb-8">Itineraries</h2>
+
+                
+
+            </div>
+
         </div>
     )
 }
